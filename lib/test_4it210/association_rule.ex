f6143eac36679cf8416ed19f306e5be6c97eb524
Jiří Vrba <jiri@vrba.dev>
2021-11-03 20:43:16 +0100

    Add association rule questions generator

---
+++ lib/test_4it210/association_rule.ex
@@ -0,0 +1,66 @@
+defmodule Test4it210.AssociationRule do
+
+  defmodule ContingencyTable do
+    @type t :: %__MODULE__{
+      a_c: integer(),
+      a_not_c: integer(),
+      not_a_c: integer(),
+      not_a_not_c: integer()
+    }
+    @enforce_keys [:a_c, :a_not_c, :not_a_c, :not_a_not_c]
+    defstruct [:a_c, :a_not_c, :not_a_c, :not_a_not_c]
+  end
+
+  defmodule AssociationRuleQuestion do
+    @type t :: %__MODULE__{
+      contingency_table: ContingencyTable.t(),
+      confidence: float(),
+      support: float(),
+      lift: float()
+    }
+    @enforce_keys [:contingency_table, :confidence, :support, :lift]
+    defstruct [:contingency_table, :confidence, :support, :lift]
+  end
+
+  @spec generate_association_rule_question() :: AssociationRuleQuestion.t()
+  def generate_association_rule_question() do
+    contingency_table = generate_contingency_table()
+
+    %AssociationRuleQuestion{
+      contingency_table: contingency_table,
+      confidence: compute_confidence(contingency_table),
+      support: compute_support(contingency_table),
+      lift: compute_lift(contingency_table)
+    }
+  end
+
+  @spec compute_confidence(ContingencyTable.t()) :: float()
+  defp compute_confidence(%ContingencyTable{a_c: a_c, not_a_not_c: not_a_not_c} = table) do
+      (a_c + not_a_not_c) / compute_total(table)
+  end
+
+  @spec compute_support(ContingencyTable.t()) :: float()
+  defp compute_support(%ContingencyTable{a_c: a_c} = table) do
+    a_c / compute_total(table)
+  end
+
+  @spec compute_lift(ContingencyTable.t()) :: float()
+  defp compute_lift(%ContingencyTable{a_c: a_c, a_not_c: a_not_c, not_a_c: not_a_c} = table) do
+    (a_c / (a_c + a_not_c)) / ((a_c + not_a_c) / compute_total(table))
+  end
+
+  @spec compute_total(ContingencyTable.t()) :: integer()
+  defp compute_total(%ContingencyTable{a_c: a_c, a_not_c: a_not_c, not_a_c: not_a_c, not_a_not_c: not_a_not_c}) do
+    a_c + a_not_c + not_a_c + not_a_not_c
+  end
+
+  @spec generate_association_rule_question() :: ContingencyTable.t()
+  defp generate_contingency_table() do
+    %ContingencyTable{
+      a_c: Enum.random(1..300),
+      a_not_c: Enum.random(1..300),
+      not_a_c: Enum.random(1..300),
+      not_a_not_c: Enum.random(1..300),
+    }
+  end
+end
--- lib/test_4it210_web/controllers/page_controller.ex
+++ lib/test_4it210_web/controllers/page_controller.ex
@@ -3,9 +3,11 @@ defmodule Test4it210Web.PageController do
 
   def index(conn, _params) do
     entropy_question = Test4it210.Entropy.generate_entropy_question()
+    assocation_rule_question = Test4it210.AssociationRule.generate_association_rule_question()
 
     conn
     |> assign(:entropy_question, entropy_question)
+    |> assign(:association_rule_question, assocation_rule_question)
     |> render("index.html")
   end
 end
