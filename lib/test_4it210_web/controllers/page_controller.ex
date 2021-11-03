cb7023f903b81270f052225480cf69bdff1c0425
Jiří Vrba <jiri@vrba.dev>
2021-11-03 20:20:04 +0100

    Add entropy and information gain question

--- lib/test_4it210_web/controllers/page_controller.ex
+++ lib/test_4it210_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule Test4it210Web.PageController do
   use Test4it210Web, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    entropy_question = Test4it210.Entropy.generate_entropy_question()
+
+    conn
+    |> assign(:entropy_question, entropy_question)
+    |> render("index.html")
   end
 end
