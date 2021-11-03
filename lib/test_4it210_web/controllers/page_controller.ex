055e84103a16fd64d12eeb401220f721fcf10f86
Jiří Vrba <jiri@vrba.dev>
2021-11-03 21:24:37 +0100

    Add confusion matrix questions generator

--- lib/test_4it210_web/controllers/page_controller.ex
+++ lib/test_4it210_web/controllers/page_controller.ex
@@ -1,13 +1,19 @@
 defmodule Test4it210Web.PageController do
   use Test4it210Web, :controller
 
+  import Test4it210.AssociationRule
+  import Test4it210.ConfusionMatrix
+  import Test4it210.Entropy
+
   def index(conn, _params) do
-    entropy_question = Test4it210.Entropy.generate_entropy_question()
-    assocation_rule_question = Test4it210.AssociationRule.generate_association_rule_question()
+    association_rule_question = generate_association_rule_question()
+    confusion_matrix_question = generate_confusion_matrix_question()
+    entropy_question = generate_entropy_question()
 
     conn
+    |> assign(:association_rule_question, association_rule_question)
+    |> assign(:confusion_matrix_question, confusion_matrix_question)
     |> assign(:entropy_question, entropy_question)
-    |> assign(:association_rule_question, assocation_rule_question)
     |> render("index.html")
   end
 end
