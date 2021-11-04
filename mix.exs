8d7ece450f8803f5d11dea7d617e926877ba9c3f
Jiří Vrba <jiri@vrba.dev>
2021-11-04 15:07:37 +0100

    Revert "Round expected results to prevent float fuckery"
    This reverts commit 7d2dacb1051ff67b01837b6564b3a81f86550dca.

--- mix.exs
+++ mix.exs
@@ -43,8 +43,7 @@ defmodule Test4it210.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:decimal, "~> 2.0"}
+      {:plug_cowboy, "~> 2.5"}
     ]
   end
 
