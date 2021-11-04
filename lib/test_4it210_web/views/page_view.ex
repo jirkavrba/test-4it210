7d2dacb1051ff67b01837b6564b3a81f86550dca
Jiří Vrba <jiri@vrba.dev>
2021-11-04 14:11:50 +0100

    Round expected results to prevent float fuckery

--- lib/test_4it210_web/views/page_view.ex
+++ lib/test_4it210_web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule Test4it210Web.PageView do
   use Test4it210Web, :view
+
+  def round_number(number, precision \\ 2) when is_float(number) do
+    number
+    |> Decimal.from_float()
+    |> Decimal.round(precision)
+    |> Decimal.to_string()
+  end
 end
